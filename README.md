3823a6c272fecc27e20d1ad7b01ef85eaefb5385
trouze <tyler@tylerrouze.com>
2023-09-01 01:28:51 -0500

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # modal-dbt
+This repo gives some code to run dbt jobs/actions using [modal](modal.com) which is a serverless application framework.
+
+deploy the run_dbt stub via:
+
+```
+modal deploy dbt_job.py
+```
+
+You'll then need to add [secrets](https://modal.com/docs/guide/secrets) to Modal for database connection credentials (they get passed as env_vars to my [profiles.yml](https://github.com/trouze/dbt-slim-ci/blob/main/profiles.yml)). You should add a web endpoint authentication token as we'll use this to authenticate requests to our run_dbt function triggered via a curl request.
+
+You can then authenticate and trigger your modal dbt job application from anywhere, whether that be python invocation in an orchestration tool, or a simple curl in your unix shell.
+
+```
+curl --header "Authorization: Bearer my-auth-token" 'https://trouze--run-dbt-demo-run-dbt.modal.run?repo_url=https://github.com/trouze/dbt-slim-ci.git&dbt_commands=run,-s,stg_customers'
+```
---
+++ run_dbt.py
@@ -0,0 +1,60 @@
+from modal import Stub, web_endpoint
+import modal
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+from fastapi import Depends, Request
+
+dbt_img = modal.Image.debian_slim().pip_install("dbt-snowflake","GitPython","fastapi").apt_install("git")
+
+auth_scheme = HTTPBearer()
+volume = modal.SharedVolume()
+stub = modal.Stub("run-dbt-demo")
+
+@stub.function(image=dbt_img,shared_volumes={"/app/dbt": volume},secret=modal.Secret.from_name("a8snowflake"))
+@web_endpoint()
+def run_dbt(request: Request, token: HTTPAuthorizationCredentials = Depends(auth_scheme)):
+    from fastapi import HTTPException, status
+    import git
+    from dbt.cli.main import dbtRunner, dbtRunnerResult
+    import os, shutil, re
+
+    # check that request is authenticated ow return http 401
+    if token.credentials != os.environ["TOKEN"]:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect bearer token",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+    repo_url = request.query_params["repo_url"]
+    dbt_commands = request.query_params["dbt_commands"]
+    dbt_commands_list = [str(item) for item in dbt_commands.split(',')] if dbt_commands else []
+    # get repository name
+    repo_name = re.match(r'https://github.com/(.*)/.*', repo_url).group(1)
+    save_path = f'/app/dbt/{repo_name}'
+
+    # handle repo removal, clone, and change directory to cloned repo
+    shutil.rmtree(save_path,ignore_errors=True)
+    repo = git.Repo.clone_from(
+        url = repo_url,
+        to_path = save_path,
+        multi_options = [
+            "--depth=1",
+            "--single-branch",
+            "--branch=main"
+        ]
+    )
+    os.chdir(save_path)
+
+    # run dbt on cloned project
+    dbt = dbtRunner()
+    deps: dbtRunnerResult = dbt.invoke(['deps'])
+    res: dbtRunnerResult = dbt.invoke(dbt_commands_list)
+    if res.success == False:
+        print("dbt invocation failed")
+        print(res.exception)
+    else:
+        print(res.result)
+
+if __name__ == "__main__":
+    with stub.run():
+        run_dbt.call()
